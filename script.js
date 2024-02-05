840c3e659c3274aa6634de51acc97ac3e33ec696
Suertle <teeraphat.btt@gmail.com>
2024-02-05 17:21:05 +0700

    add preview case

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
+var interval = 5000;
+
 setInterval(function() {
   var skb = document.querySelector(".ytp-ad-skip-button-modern");
+  var pvb = document.querySelector(".ytp-ad-preview-text-modern");
+
   if (skb) {
-    console.log("Found");
+    console.log("Found (Skip)");
     skb.click();
-    console.log("Click");
+    console.log("Click Skip");
+  } else if (pvb) {
+    console.log("Found (Preview)");
+    document.querySelector("video").currentTime = document.querySelector("video").duration;
+    console.log("End Video");
   } else {
-    console.log('Not Found')
+    console.log('Not Found');
   }
-}, 5000);
+}, interval);
