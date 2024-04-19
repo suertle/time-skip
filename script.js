a0d44d696d30b270c3977f99fb7e70a133cd9db6
Suertle <teeraphat.btt@gmail.com>
2024-04-19 15:45:52 +0700

    update class

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 var interval = 1000;
 
 setInterval(function() {
-  var skb = document.querySelector(".ytp-ad-skip-button-modern");
+  var skbm = document.querySelector(".ytp-ad-skip-button-modern");
+  var skb = document.querySelector(".ytp-skip-ad-button");
   var pvb = document.querySelector(".ytp-ad-preview-text-modern");
 
-  if (skb) {
-    console.log("Found (Skip)");
-    skb.click();
-    console.log("Click Skip");
+  if (skbm) {
+    console.log("Found (Skip Modern)");
+    skbm.click();
+    console.log("Click Skip Modern");
+  } else if (skb) {
+      console.log("Found (Skip)");
+      skb.click();
+      console.log("Click Skip");
   } else if (pvb) {
     console.log("Found (Preview)");
     document.querySelector("video").currentTime = document.querySelector("video").duration;
