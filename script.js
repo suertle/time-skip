2342042870b8c95a5bf06a4163d6820df87bdeed
Suertle <teeraphat.btt@gmail.com>
2024-06-25 11:04:25 +0700

    update delay

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
+var found = false
 var interval = 1000;
+var delay = 500;
 
 setInterval(function() {
-  var skbm = document.querySelector(".ytp-ad-skip-button-modern");
   var skb = document.querySelector(".ytp-skip-ad-button");
-  var pvtm = document.querySelector(".ytp-ad-preview-text-modern");
   var pva = document.querySelector(".ytp-preview-ad");
 
-  if (skbm) {
-    console.log("Found (Skip Modern)");
-    skbm.click();
-    console.log("Click Skip Modern");
-  } else if (skb) {
-      console.log("Found (Skip)");
-      skb.click();
-      console.log("Click Skip");
-  } else if (pvtm || pva) {
-    console.log("Found (Preview)");
-    document.querySelector("video").currentTime = document.querySelector("video").duration;
-    console.log("End Video");
+  if (! found && (skb || pva)) {
+    found = true
+    setTimeout(function() {
+      if (skb) {
+        console.log("Found (Skip)");
+        skb.click();
+      } else if (pva) {
+        console.log("Found (Preview)");
+        document.querySelector("video").currentTime = document.querySelector("video").duration;
+      }
+      found = false
+    }, delay)
   } else {
     console.log('Not Found');
   }
