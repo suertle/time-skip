9ce65ac54b2e302baee6356156abae75cd9d6124
Suertle <teeraphat.btt@gmail.com>
2024-08-06 11:55:07 +0700

    update selector

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 var found = false
 var interval = 500;
-var delay = 1000;
+var delay = 5000;
 
 setInterval(function() {
   var skb = document.querySelector(".ytp-skip-ad-button");
-  var pva = document.querySelector(".ytp-preview-ad");
+  var pva = document.querySelector("[id^='ad-preview']");
 
-  if (! found && (skb || pva)) {
+  if (! found && pva) {
     found = true
     setTimeout(function() {
-      if (skb) {
-        console.log("Found (Skip)");
-        skb.click();
-      } else if (pva) {
+      if (pva) {
         console.log("Found (Preview)");
         document.querySelector("video").currentTime = document.querySelector("video").duration;
       }
